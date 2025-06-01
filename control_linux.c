a177e806f0f4bb9bba4f788aff58108b464d4d6b
jedl472 <jodltomas6@gmail.com>
2025-06-01 20:29:42 +0200

    Added control for linux

---
+++ control_linux.c
@@ -0,0 +1,56 @@
+#include "control_linux.h"
+
+#ifndef UNISTD_H
+#define UNISTD_H 
+#include <unistd.h>
+#endif
+
+#ifndef TERMIOS_H
+#define TERMIOS_H
+#include <termios.h>
+#endif
+
+void enable_raw_mode(struct termios *orig_termios) {
+    struct termios raw;
+
+    // Get current terminal attributesn
+    tcgetattr(STDIN_FILENO, orig_termios);
+
+    raw = *orig_termios;
+
+    // Disable canonical mode and echo
+    raw.c_lflag &= ~(ICANON | ECHO);
+
+    // Apply the new attributes
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+}
+
+void disable_raw_mode(const struct termios *orig_termios) {
+    // Restore original terminal attributes
+    tcsetattr(STDIN_FILENO, TCSANOW, orig_termios);
+}
+
+void control_loop_invoker(void (*invoked_function)(char)) {
+  struct termios orig_termios;
+  char ch;
+
+  enable_raw_mode(&orig_termios);
+  while (1) {
+      if (read(STDIN_FILENO, &ch, 1) == 1) {
+          /*if (ch == 27) { // ESC key
+              printf("\nESC pressed. Exiting...\n");
+              break;
+          }
+
+          if (ch >= 32 && ch <= 126) {
+              printf("You pressed: '%c'\n", ch);
+          } else {
+              printf("You pressed a control key (ASCII code: %d)\n", ch);
+          }*/
+        invoked_function(ch);
+      }
+  }
+
+  disable_raw_mode(&orig_termios);
+}
+
---
+++ control_linux.h
@@ -0,0 +1,7 @@
+#ifndef CONTROL_LINUX_H
+
+#define CONTROL_LINUX_H
+
+void control_loop_invoker(void (*invoked_function)(char));
+
+#endif
