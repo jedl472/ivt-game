788663043d06f47401eb32ca5eaeee25f2f25618
jedl472 <jodltomas6@gmail.com>
2025-06-11 20:40:25 +0200

    fix

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc main.c control_linux.c renderer.c -o main.out -Wall -Wextra --std=c23 -pedantic -lm 
+clear && gcc main.c control_linux.c renderer.c -o main.out -Wall -Wextra --std=c23 -pedantic -lm && valgrind ./main.out
